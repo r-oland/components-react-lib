4c192be562e6dde997b1ff4f243c1d258c97e11e
Roland Branten <roland.branten@gmail.com>
2020-05-04 14:05:38 +0200

    removed styled-system

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,16 +1,23 @@
 import { motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
-import { color } from "styled-system";
 
 const Test = styled(motion.div)`
-  ${color}
+  color: ${({ theme: { white } }) => white};
+  text-align: left;
 `;
 
-export const ExampleComponent = ({ bg, color }) => {
+export const ExampleComponent = () => {
   return (
-    <Test animate={{ y: 80 }} bg={bg} color={color}>
-      test
+    <Test animate={{ y: -80 }}>
+      <i>Component lib</i>
+      <p>npm link [path]/node_modules/react</p>
+      <p>npm link [path]/node_modules/styled-components</p>
+      <p>npm link </p>
+      <br />
+      <i>App</i>
+      <p>npm i components-react-link</p>
+      <p>npm link components-react-link</p>
     </Test>
   );
 };
